2451d745d93997684461b49d2c6d6dfe041410b7
elieboulingui <elieboulingui2@gmail.com>
2024-08-06 11:19:25 +0100

    j ai arrange les bugs et j ai termine

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         <nav className="flex justify-between items-center p-4 relative">
           <a href="#" className="link-styled">Eivent</a>
           <div className="lg:hidden flex items-center z-10">
-            <button onClick={toggleMenu} className="p-2 rounded-md text-purple-700 hover:bg-purple-100">
+            <button onClick={toggleMenu} className="p-2 rounded-md custom-bg hover:bg-purple-100">
               <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
               </svg>
@@ -24,7 +24,7 @@ export default function Home() {
             <li className="text-lg font-medium text-white p-2 lg:p-0">About</li>
             <li className="text-lg font-medium text-white p-2 lg:p-0">Space</li>
             <li className="text-lg font-medium text-white p-2 lg:p-0">Contact</li>
-            <button className="bg-white text-purple-700 py-1 px-4 rounded hover:bg-purple-100">
+            <button className="bg-white custom-bg py-1 px-4 rounded hover:bg-purple-100">
               BYU Tickets
             </button>
           </ul>
@@ -105,7 +105,7 @@ export default function Home() {
                 </p>
               </div>
               <div className='flex justify-center pt-4 pr-10'>
-                <button className="bg-violet-400 text-white px-4 py-2 rounded hover:bg-purple-100">
+                <button className="custom-bg text-white px-4 py-2 rounded hover:bg-purple-100">
                   BYU Tickets
                 </button>
               </div>
@@ -113,7 +113,7 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="h-full bg-violet-600 flex flex-col justify-around">
+      <div className="h-full custom-bg flex flex-col justify-around">
         <div className="flex-grow flex items-center justify-center text-center pt-4">
           <div>
             <h1 className="text-white text-2xl mb-4">
@@ -172,12 +172,12 @@ export default function Home() {
           </div>
         </div>
         <div className='flex justify-center pt-4'>
-          <button className="bg-violet-400 text-white px-4 py-2 rounded hover:bg-purple-100">
+          <button className="custom-bg text-white px-4 py-2 rounded hover:bg-purple-100">
             BYU Tickets
           </button>
         </div>
       </div>
-      <div className="h-full bg-violet-600 flex flex-col justify-around">
+      <div className="h-full custom-bg flex flex-col justify-around">
         <div className="flex-grow flex items-center justify-center text-center pt-4">
           <div>
             <h1 className="text-white text-2xl mb-4">
@@ -308,7 +308,7 @@ export default function Home() {
       </div>
       <div className='h-full pb-10 bg-slate-50  pt-10 flex flex-col justify-around' >
         <div className='flex flex-row justify-center'>
-          <div className='h-96 bg-violet-800 w-96'>
+          <div className='h-96 custom-bg w-96'>
             <div>
               <h1>Venue of meeting <br /> Conference </h1>
               <p className='text-white font-thin pt-5'>   Join us for dinner and networking. This is fully responsive</p>
@@ -328,7 +328,7 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className='h-full bg-violet-600  pt-10 flex flex-col justify-around'>
+      <div className='h-full custom-bg  pt-10 flex flex-col justify-around'>
         <div className="flex-grow flex items-center justify-center text-center pt-4">
           <div>
             <h1 className="text-white text-2xl mb-4">
@@ -368,73 +368,78 @@ export default function Home() {
             </div>
           </div>
           <div className='flex justify-center pt-4'>
-            <button className="bg-violet-400 text-white px-4 py-2 rounded hover:bg-purple-100">
+            <button className="custom-bg  text-white px-4 py-2 rounded hover:bg-purple-100">
               BYU Tickets
             </button>
           </div>
         </div>
       </div>
       <div className='bg-slate-50 flex flex-col min-h-screen relative'>
-        <div className='bg-red-700 flex-shrink-0'>
-          <a href="https://www.google.fr/maps/preview" target="_blank" rel="noopener noreferrer">
-            <img src="./image/map.avif" alt="Map" className="w-full max-h-80 object-cover" />
-          </a>
-        </div>
+ 
+  <div className='bg-red-700 flex-shrink-0'>
+    <a href="https://www.google.fr/maps/preview" target="_blank" rel="noopener noreferrer">
+      <img src="./image/map.avif" alt="Map" className="w-full max-h-80 object-cover" />
+    </a>
+  </div>
 
-        <div className='absolute inset-x-0 top-1/2 transform -translate-y-1/2 flex items-start justify-center space-x-0'>
-          <div className='bg-violet-600 p-7 shadow-lg max-w-md w-full'>
-            <h1>Get in touch</h1>
-            <div className='flex items-start w-64 max-w-xs'>
-  <div className='w-2 bg-gray-600 mr-4'></div>
-  <div>
-    <p className='text-sm pt-4'>Lorem ipsum dolor, sit amet consectetur consequuntur <br /> repellendus ducimus magnam <br /> accusamus fugit vitae!</p>
-    <p className='text-sm pt-4'>Lorem ipsum dolor, sit amet consectetur consequuntur <br /> repellendus ducimus magnam <br /> accusamus fugit vitae!</p>
-    <p className='text-sm pt-8'>Lorem ipsum dolor, sit amet consectetur consequuntur accusamus fugit vitae!</p>
+
+  <div className='absolute inset-x-0 top-1/2 transform -translate-y-1/2 flex flex-col md:flex-row items-start justify-center space-y-6 md:space-y-0 md:space-x-6'>
+
+    <div className='custom-bg p-4 md:pb-16 shadow-lg max-w-md w-full'>
+      <h1 className='text-lg font-semibold text-white'>Get in touch</h1>
+      <div className='flex flex-col md:flex-row items-start'>
+        <div className='w-full md:w-2 bg-gray-600 mb-4 md:mb-0 md:mr-4'></div>
+        <div className='text-white'>
+          <p className='text-sm pt-2 md:pt-4'>Lorem ipsum dolor, sit amet consectetur consequuntur <br /> repellendus ducimus magnam <br /> accusamus fugit vitae!</p>
+          <p className='text-sm pt-2 md:pt-4'>Lorem ipsum dolor, sit amet consectetur consequuntur <br /> repellendus ducimus magnam <br /> accusamus fugit vitae!</p>
+          <p className='text-sm pt-4 md:pt-8'>Lorem ipsum dolor, sit amet consectetur consequuntur accusamus fugit vitae!</p>
+        </div>
+      </div>
+    </div>
+
+
+    <div className='custom-bg p-4 md:p-7 shadow-lg max-w-md w-full'>
+      <form>
+        <h2 className='text-lg font-semibold mb-4 text-white'>Sign Up</h2>
+        <div className='mb-4'>
+          <label htmlFor='username' className='block text-sm font-medium text-white'>Username</label>
+          <input type='text' id='username' name='username' className='mt-1 block w-full border-gray-300 rounded-md shadow-sm' />
+        </div>
+        <div className='mb-4'>
+          <label htmlFor='signup-email' className='block text-sm font-medium text-white'>Email</label>
+          <input type='email' id='signup-email' name='signup-email' className='mt-1 block w-full border-gray-300 rounded-md shadow-sm' />
+        </div>
+        <div className='mb-4'>
+          <label htmlFor='password' className='block text-sm font-medium text-white'>Password</label>
+          <input type='password' id='password' name='password' className='mt-1 block w-full border-gray-300 rounded-md shadow-sm' />
+        </div>
+        <button type='submit' className='bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600'>Sign Up</button>
+      </form>
+    </div>
+  </div>
+
+
+  <div className='bg-black h-96 flex-shrink-0'>
+    <div className='flex items-center justify-center p-4'>
+
+    </div>
+  </div>
+
+ -
+  <div className='bg-black h-20 fixed bottom-0 w-full flex-shrink-0'>
+    <div className='flex items-center justify-center h-full px-4'>
+      <ul className="flex flex-wrap justify-center space-x-6 text-white text-sm md:text-lg">
+        <li><a href="#" className='hover:underline'>Careers</a></li>
+        <li><a href="#" className='hover:underline'>About</a></li>
+        <li><a href="#" className='hover:underline'>Privacy</a></li>
+        <li><a href="#" className='hover:underline'>Policy</a></li>
+        <li><a href="#" className='hover:underline'>MobileApp</a></li>
+        <li><a href="#" className='hover:underline'>Press</a></li>
+      </ul>
+    </div>
   </div>
 </div>
 
-
-          </div>
-
-          <div className='bg-violet-900  p-11 shadow-lg max-w-md w-full'>
-            <form>
-              <h2 className='text-lg font-semibold mb-4'>Sign Up</h2>
-              <div className='mb-4'>
-                <label htmlFor='username' className='block text-sm font-medium text-white'>Username</label>
-                <input type='text' id='username' name='username' className='mt-1 block w-full border-gray-300 rounded-md shadow-sm' />
-              </div>
-              <div className='mb-4'>
-                <label htmlFor='signup-email' className='block text-sm font-medium text-white'>Email</label>
-                <input type='email' id='signup-email' name='signup-email' className='mt-1 block w-full border-gray-300 rounded-md shadow-sm' />
-              </div>
-              <div className='mb-4'>
-                <label htmlFor='password' className='block text-sm font-medium text-white'>Password</label>
-                <input type='password' id='password' name='password' className='mt-1 block w-full border-gray-300 rounded-md shadow-sm' />
-              </div>
-              <button type='submit' className='bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600'>Sign Up</button>
-            </form>
-          </div>
-        </div>
-
-        <div className='bg-black h-96 flex-shrink-0'>
-          <div className='flex items-center justify-center p-4'>
-          </div>
-        </div>
-        <div className='bg-black h-20 fixed bottom-0 w-full flex-shrink-0'>
-          <div className='flex items-center justify-center h-full px-4'>
-            <ul className="flex flex-row space-x-6 text-white text-lg">
-              <li><a href="#" className='hover:underline'>Careers</a></li>
-              <li><a href="#" className='hover:underline'>About</a></li>
-              <li><a href="#" className='hover:underline'>Privacy</a></li>
-              <li><a href="#" className='hover:underline'>Policy</a></li>
-              <li><a href="#" className='hover:underline'>MobileApp</a></li>
-              <li><a href="#" className='hover:underline'>Press</a></li>
-            </ul>
-          </div>
-        </div>
-
-
-      </div>
 
     </main>
   );
