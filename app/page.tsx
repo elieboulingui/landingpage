f3f9fa22f90410ce96d522efcffe90e6557ebe1c
elieboulingui <elieboulingui2@gmail.com>
2024-08-06 06:59:13 +0100

    j ai termine avec le venue of meeting

--- app/page.tsx
+++ app/page.tsx
@@ -143,11 +143,7 @@ export default function Home() {
               ne constituant pas l'intrigue principale d'une œuvre. <br />
               Ce terme est notamment utilisé dans le domaine des jeux vidéo.
             </p>
-            <div className="pt-4 flex justify-center">
-              <svg xmlns="http://www.w3.org/2000/svg" className="w-8 h-8 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 3l14 9-14 9V3z" />
-              </svg>
-            </div>
+
           </div>
         </div>
       </div>
@@ -195,51 +191,142 @@ export default function Home() {
           </div>
         </div>
         <div className="flex justify-around p-4">
+          <div className='flex flex-col w-full max-w-7xl'>
+            <div className='pl-4 md:pl-20 md:pb-11 relative pt-5'>
+              <div className="flex justify-between items-start">
+                <span className="mr-8   text-white relative">23 <br /> May
+                  <span className="absolute left-1/2 transform -translate-x-1/2 w-px bg-white top-full h-[20px] md:h-12"></span>
+                </span>
+                <div className="flex flex-col justify-between items-start mr-4">
+                  <p className="mr-10 text-white">Lunch</p>
+                  <p className='text-white'>03:30 pm</p>
+                </div>
+                <p className="text-xs md:text-base link-styled text-white pl-4">
+                  Another exciting event description goes here. This is fully
+                </p>
+              </div>
+            </div>
+            <div className='pl-4 md:pl-20 md:pb-11 relative pt-5'>
+              <div className="flex justify-between items-start">
+                <span className="mr-8   text-white relative">23 <br /> May
+                  <span className="absolute left-1/2 transform -translate-x-1/2 w-px bg-white top-full h-[20px] md:h-12"></span>
+                </span>
+                <div className="flex flex-col justify-between items-start mr-4">
+                  <p className="mr-10 text-white">Lunch</p>
+                  <p className='text-white'>03:30 pm</p>
+                </div>
+                <p className="text-xs md:text-base link-styled text-white pl-4">
+                  Another exciting event description goes here. This is fully
+                </p>
+              </div>
+            </div>  <div className='pl-4 md:pl-20 md:pb-11 relative pt-5'>
+              <div className="flex justify-between items-start">
+                <span className="mr-8   text-white relative">23 <br /> May
+                  <span className="absolute left-1/2 transform -translate-x-1/2 w-px bg-white top-full h-[20px] md:h-12"></span>
+                </span>
+                <div className="flex flex-col justify-between items-start mr-4">
+                  <p className="mr-10 text-white">Lunch</p>
+                  <p className='text-white'>03:30 pm</p>
+                </div>
+                <p className="text-xs md:text-base link-styled text-white pl-4">
+                  Another exciting event description goes here. This is fully
+                </p>
+              </div>
+            </div>  <div className='pl-4 md:pl-20 md:pb-11 relative pt-5'>
+              <div className="flex justify-between items-start">
+                <span className="mr-8   text-white relative">23 <br /> May
+                  <span className="absolute left-1/2 transform -translate-x-1/2 w-px bg-white top-full h-[20px] md:h-12"></span>
 
-  <div className='flex flex-col w-full max-w-md'>
-    <div className='pt-6 pl-4 md:pl-20 md:pb-11'>
-      <div className="flex justify-between items-start">
-        <span className="mr-4">22 <br /> May</span>
-        <div className="flex flex-col justify-between items-start mr-4 mb-4 ">
-          <p  className="mr-10" >Breakfast</p>
+                </span>
+                <div className="flex flex-col justify-between items-start mr-4">
+                  <p className="mr-10 text-white">Lunch</p>
+                  <p className='text-white'>03:30 pm</p>
+                </div>
+                <p className="text-xs md:text-base link-styled text-white pl-4">
+                  Another exciting event description goes here. This is fully
+                </p>
+              </div>
+            </div>  <div className='pl-4 md:pl-20 md:pb-11 relative pt-5'>
+              <div className="flex justify-between items-start">
+                <span className="mr-8   text-white relative">23 <br /> May
+                  <span className="absolute left-1/2 transform -translate-x-1/2 w-px bg-white top-full h-[20px] md:h-12"></span>
+
+                </span>
+                <div className="flex flex-col justify-between items-start mr-4">
+                  <p className="mr-10 text-white">Lunch</p>
+                  <p className='text-white'>03:30 pm</p>
+                </div>
+                <p className="text-xs md:text-base link-styled text-white pl-4">
+                  Another exciting event description goes here. This is fully
+                </p>
+              </div>
+            </div>  <div className='pl-4 md:pl-20 md:pb-11 relative pt-5'>
+              <div className="flex justify-between items-start">
+                <span className="mr-8   text-white relative">23 <br /> May
+                  <span className="absolute left-1/2 transform -translate-x-1/2 w-px bg-white top-full h-[20px] md:h-12"></span>
+
+                </span>
+                <div className="flex flex-col justify-between items-start mr-4">
+                  <p className="mr-10 text-white">Lunch</p>
+                  <p className='text-white'>03:30 pm</p>
+                </div>
+                <p className="text-xs md:text-base link-styled text-white pl-4">
+                  Another exciting event description goes here. This is fully
+                </p>
+              </div>
+            </div>  <div className='pl-4 md:pl-20 md:pb-11 relative pt-5'>
+              <div className="flex justify-between items-start">
+                <span className="mr-8   text-white relative">23 <br /> May
+                  <span className="absolute left-1/2 transform -translate-x-1/2 w-px bg-white top-full h-[20px] md:h-12"></span>
+
+                </span>
+                <div className="flex flex-col justify-between items-start mr-4">
+                  <p className="mr-10 text-white">Lunch</p>
+                  <p className='text-white'>03:30 pm</p>
+                </div>
+                <p className="text-xs md:text-base link-styled text-white pl-4">
+                  Another exciting event description goes here. This is fully
+                </p>
+              </div>
+            </div>
+
+            <div className='pl-4 md:pl-20 md:pb-11 relative pt-5'>
+              <div className="flex justify-between items-start">
+                <span className="mr-8 text-white relative">24 <br /> May
+                </span>
+                <div className="flex flex-col justify-between items-start mr-4">
+                  <p className="mr-10  text-white">Dinner</p>
+                  <p className='text-white'>03:30 pm</p>
+                </div>
+                <p className="text-xs text-white md:text-base link-styled pl-4">
+                  Join us for dinner and networking. This is fully responsive
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
-        <p className="text-xs md:text-base link-styled ">
-          We are so excited to introduce to you our new Unbounce
-          Template called Conference. This Template is fully
-          responsive and CMS ready, no coding skills required!
-        </p>
       </div>
-    </div>
-
-    <div className='pl-4 md:pl-20 md:pb-11 mt-6'>
-      <div className="flex justify-between items-start">
-        <span className="mr-4">23 <br /> May</span>
-        <div className="flex flex-col justify-between items-start mr-4 mb-4">
-          <p className="mr-10">Lunch</p>
+      <div className='h-full bg-slate-50  pt-10 flex flex-col justify-around' >
+        <div className='flex flex-row justify-center'>
+          <div className='h-96 bg-violet-800 w-96'>
+            <div>
+              <h1>Venue of meeting <br /> Conference </h1>
+              <p className='text-white font-thin pt-5'>   Join us for dinner and networking. This is fully responsive</p>
+            </div>
+            <div className=' flex justify-center pt-10 pl-7 flex-col '>
+              <h1 className=''>Location</h1>
+              <p className='text-white font-thin '>   Join us for dinner and networking. This is fully responsive</p>
+            </div>
+            <div className=' flex justify-center pt-10 pl-7 flex-col '>
+              <h1 className=''>Location</h1>
+              <p className='text-white font-thin '>   Join us for dinner and networking. This is fully responsive</p>
+            </div>
+          </div>
+          <div className='h-96 bg-white-800 w-96 flex flex-col'>
+            <div><img src="./image/téléchargement.jpeg" alt="" /></div>
+            <div><img src="./image/téléchargement.jpeg" alt="" /></div>
+          </div>
         </div>
-        <p className="text-xs md:text-base link-styled pl-4">
-          Another exciting event description goes here. This is fully
-          responsive and CMS ready, no coding skills required!
-        </p>
-      </div>
-    </div>
-
-    <div className='pl-4 md:pl-20 md:pb-11 mt-6'>
-      <div className="flex justify-between items-start">
-        <span className="mr-4">24 <br /> May</span>
-        <div className="flex flex-col justify-between items-start mr-4 mb-4">
-          <p className="mr-10">Dinner</p>
-        </div>
-        <p className="text-xs md:text-base link-styled pl-4">
-          Join us for dinner and networking. This is fully responsive
-          and CMS ready, no coding skills required!
-        </p>
-      </div>
-    </div>
-  </div>
-</div>
-
-
       </div>
     </main>
   );
