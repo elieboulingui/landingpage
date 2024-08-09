db43280e261f2f7a453656cca666976c1ff87e08
elieboulingui <elieboulingui2@gmail.com>
2024-08-09 14:10:47 +0100

    j ai fixe l 'erreur a schedule

--- app/components/Schedule.tsx
+++ app/components/Schedule.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
- const  Schedule:  React.FC =() =>{
+const Schedule: React.FC = () => {
     return (
         <div className="h-full  container mx-auto bg-violet-600 flex flex-col justify-between p-4">
             <div className="flex-grow flex items-center justify-center text-center pt-4">
@@ -16,17 +16,19 @@ import React from 'react';
                 </div>
             </div>
 
-            <div className="flex flex-col items-center p-4 space-y-4">
-                {Array(6).fill().map((_, index) => (
+            <div className="flex flex-col items-center p-4 space-y-14 ">
+                {Array(6).fill(null).map((_, index) => (
                     <div key={index} className="flex flex-col md:flex-row items-start  p-4 rounded-lg shadow-lg w-full max-w-3xl">
                         <div className="relative flex flex-col pr-4 md:pr-10 justify-between mr-4 w-[78px] h-[43px]">
-                            <span className="text-lg md:text-xl text-white">26</span>
-                            <p className="text-xs text-white i  absolute ml-9 pt-3" >May</p>
+                            <span className="text-lg md:text-4xl text-white">26</span>
+                            <p className="text-xs text-white md:text-xl   absolute pt-11" >May</p>
+                            <p className="text-xs text-white md:text-xl   absolute ml-20 pt-3" >May</p>
+                            <p className='text-xs text-white  md:text  absolute ml-20 pt-11'>08:30 AM</p>
                             <div className="absolute bottom-0 left-0 pl-4 md:pl-14">
                             </div>
                         </div>
-                        <div className="flex-grow">
-                            <p className="text-white text-sm md:text-xs ml-0 md:ml-12">
+                        <div className="flex-grow pl-44">
+                            <p className="text-white text-sm md:text-xs ml-0 md:ml-12 px-24 py-3">
                                 We are so excited to introduce to you our new Unbounce
                                 Template called Conference. This Template is fully responsive
                                 and CMS ready, no coding skills required!
@@ -34,8 +36,9 @@ import React from 'react';
                         </div>
                     </div>
                 ))}
-            </div></div>
+            </div>
+        </div>
 
     );
 }
-export default   Schedule;
+export default Schedule;
